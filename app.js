f174b809428f38ddd2a788294e995a2c59185b10
Nithin3008 <60174062+Nithin3008@users.noreply.github.com>
2022-03-15 10:32:29 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var txt1=document.querySelector("#text-area1")
+var txt2=document.querySelector("#text-area2")
+var btn2=document.querySelector("#btn1")
+var url="https://api.funtranslations.com/translate/ferb-latin.json";
+
+function setUrl(text)
+{
+    return url+"?"+"text="+text;
+}
+
+function trans()
+{
+    var inputTex=txt1.value
+    // var url1=setUrl(inputTex)
+    fetch(setUrl(inputTex))
+    .then(response=> response.json())
+    .then(json =>{
+        console.log(json.contents.translated);
+    txt2.innerText = json.contents.translated;
+    })
+}
+
+btn2.addEventListener("click",trans)
